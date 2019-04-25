22c87aea6e171358bb305305f69a833f061d51f6
yaoweifeng01 <yaoweifeng01@baidu.com>
2019-04-25 16:21:36 +0800

    add whole source/

--- docs-sphinx/source/conf.py
+++ docs-sphinx/source/conf.py
@@ -1,55 +1,38 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# -- Path setup --------------------------------------------------------------
+import os
+import subprocess
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
+if on_rtd:
+    subprocess.call('cd ..; doxygen', shell=True)
 
-# -- Project information -----------------------------------------------------
+import sphinx_rtd_theme
 
-project = 'myprj3'
-copyright = '2019, ywf'
-author = 'ywf'
+html_theme = "sphinx_rtd_theme"
 
-# The full version, including alpha/beta/rc tags
-release = '1/0/1'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+def setup(app):
+    app.add_stylesheet("main_stylesheet.css")
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-]
-
-# Add any paths that contain templates here, relative to this directory.
+extensions = ['breathe']
+breathe_projects = { 'xtensor': '../xml' }
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'xtensor'
+copyright = '2016, Johan Mabille, Sylvain Corlay and Wolf Vollprecht'
+author = 'Johan Mabille, Sylvain Corlay and Wolf Vollprecht'
+
+html_logo = 'quantstack-white.svg'
+
+exclude_patterns = []
+highlight_language = 'c++'
+pygments_style = 'sphinx'
+todo_include_todos = False
+htmlhelp_basename = 'xtensordoc'
+
